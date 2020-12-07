7bf596a65b763f9c0e935b5406657ddc739a208c
Justin Cheasty <cheasty@gmail.com>
2020-12-07 15:08:18 -0800

    Created next move nodes

--- w4d1ttt/lib/tic_tac_toe_node.rb
+++ w4d1ttt/lib/tic_tac_toe_node.rb
@@ -6,9 +6,16 @@ class TicTacToeNode
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
+    @next_player = nil
+      if next_mover_mark == :x
+        next_player = :o 
+      else
+        next_player = :x
+      end
   end
 
   def losing_node?(evaluator)
+    return true if board.over? && !board.winner
   end
 
   def winning_node?(evaluator)
@@ -17,13 +24,22 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-    current_board = @board.dup
     empty_pos = []
     @board.each_with_index do |row, i1|
       row.each_with_index do |position, i2|
         empty_pos << [i1, i2] if @board.empty?([i1, i2])
       end
     end
-    empty_pos
+    empty_pos.each do |e_pos|
+      current_board = @board.dup
+      current_board.[]=(e_pos, next_mover_mark)
+      possible_move = TicTacToeNode.new(current_board, next_player, e_pos)
+    end
   end
+
+
+
+
+
+
 end
