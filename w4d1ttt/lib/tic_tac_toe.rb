cc90529c52f0dcc940ce45e4b5665d8045c25c3b
Andrew Curd <andrewc2695@gmail.com>
2020-12-07 14:49:07 -0800

    Started empty positions checker method

--- w4d1ttt/lib/tic_tac_toe.rb
+++ w4d1ttt/lib/tic_tac_toe.rb
@@ -212,3 +212,4 @@ if __FILE__ == $PROGRAM_NAME
 
   TicTacToe.new(hp, cp).run
 end
+
--- w4d1ttt/lib/tic_tac_toe_node.rb
+++ w4d1ttt/lib/tic_tac_toe_node.rb
@@ -1,7 +1,11 @@
 require_relative 'tic_tac_toe'
+attr_reader :board, :next_mover_mark, :prev_move_pos
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,13 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    current_board = @board.dup
+    empty_pos = []
+    @board.each_with_index do |row, i1|
+      row.each_with_index do |position, i2|
+        empty_pos << [i1, i2] if @board.empty?([i1, i2])
+      end
+    end
+    empty_pos
   end
 end
