e1f820c7a0be7586a3ee931dbc1042d68a716cbd
Qi Zhu <rubybfm1017@gmail.com>
2020-12-17 14:58:51 -0800

    user.rb

--- W5D4/skeleton/app/models/user.rb
+++ W5D4/skeleton/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :Enrollment
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
