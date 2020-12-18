c38c27025009656565cb28a77cab3625862127ae
Qi Zhu <rubybfm1017@gmail.com>
2020-12-17 17:02:22 -0800

    classwork w5d4

--- W5D4/skeleton/app/models/course.rb
+++ W5D4/skeleton/app/models/course.rb
@@ -5,10 +5,28 @@ class Course < ApplicationRecord
       class_name: :Enrollment
     has_many :enrolled_students,
       through: :enrollments,
-      source: user
-    belongs_to: instructor,
+      source: :user
+
+    belongs_to :instructor,
       primary_key: :id,
       foreign_key: :instructor_id,
       class_name: :User
       
+    #null : false
+    belongs_to :prereq,
+      primary_key: :id,
+      foreign_key: :prereq_id,
+      class_name: :Course,
+      optional: :true
+
+
+    has_many :parent_classes,
+      primary_key: :id,
+      foreign_key: :prereq_id,
+      class_name: :Course
+      # null :false
 end
+#101 has parent classes, 101 , 102 ,103 
+#world history
+# history of russia
+#history of india
